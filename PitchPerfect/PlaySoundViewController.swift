7cb9d9d2afac70dded825189aad2804b53abff86
Kyle Hodgetts <kylehodgetts@gmail.com>
2015-06-11 15:53:18 +0200

    Implemented chipmunk effect using audioengine and nodes to change sound effects

--- PitchPerfect/PlaySoundViewController.swift
+++ PitchPerfect/PlaySoundViewController.swift
@@ -11,28 +11,44 @@ import AVFoundation
 
 class PlaySoundViewController: UIViewController {
     
-    private var player: AVAudioPlayer!
+    private var audioEngine: AVAudioEngine!
+    private var audioFile: AVAudioFile!
     var recievedAudio: RecordedAudio!
+
+    
+    private var playerNode: AVAudioPlayerNode!
+    private var auTimePitch: AVAudioUnitTimePitch!
+    
+    private var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Returns the file's location URL from the NSBundle (Project resources bundled)
-        //Path for resource, from the main bundle look for the file name with the given type
-//        if var fileLoc = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
-//            //Convert string fileLoc to URLPath
-//            var filePathURL = NSURL.fileURLWithPath(fileLoc)
-//            
-//        }
-//        else {
-//            println("File does not exist!")
-//        }
+        //Instantiate AudioEngine
+        audioEngine = AVAudioEngine()
+        
+        //Instantiate Audiofile to read the recieved audio via its fileUrl
+        audioFile = AVAudioFile(forReading: recievedAudio.filePathUrl, error: nil)
         
         //Instantiate the player with the contents of the filePathURL.
         player = AVAudioPlayer(contentsOfURL: recievedAudio.filePathUrl, error: nil)
         
         //Enable rate so we can change the speed at which the sound is played
         player.enableRate = true
+        
+        
+        
+        /* LEGACY CODE */
+        //Returns the file's location URL from the NSBundle (Project resources bundled)
+        //Path for resource, from the main bundle look for the file name with the given type
+        //        if var fileLoc = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+        //            //Convert string fileLoc to URLPath
+        //            var filePathURL = NSURL.fileURLWithPath(fileLoc)
+        //
+        //        }
+        //        else {
+        //            println("File does not exist!")
+        //        }
     }
     
     @IBAction func playSlowSound(sender: UIButton) {
@@ -43,8 +59,40 @@ class PlaySoundViewController: UIViewController {
         playSound(2.0)
     }
     
+    @IBAction func playChipmunkSound(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
     @IBAction func stopButtonPressed(sender: UIButton) {
         player.stop()
+    }
+    
+    private func playAudioWithVariablePitch(pitch: Float){
+        //Reset everything to 0
+        player.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        //Instantiate audioPlayerNode and attach it to the engine
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        //Instantiate the AudioTimeUnitPitch and attach it to the engine
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch     //Set pitch to argument value
+        audioEngine.attachNode(changePitchEffect)
+        
+        //connect audioplayer to pitcheffect
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        //Connect pitch effect to output, such as speakers
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        //Schedule whole file to play as soon as function is called (atTime: nil)
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        //play the audioPlayer
+        audioPlayerNode.play()
     }
     
     private func playSound(speed: Float){
